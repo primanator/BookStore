e40ab071c1b7217b563b41f5c38cc94585e4d157
Oleksii Prymolonnyi <Oleksii_Prymolonny@epam.com>
2018-09-23 18:20:22 +0300

    Adds exception filter
    Of NotImplementedException to all API controllers

--- BookStore/API/Global.asax.cs
+++ BookStore/API/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿namespace API
 {
+    using Utils;
     using System.Web.Http;
 
     public class WebApiApplication : System.Web.HttpApplication
@@ -7,6 +8,7 @@
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Filters.Add(new NotImplementedExceptionFilterAttribute());
         }
     }
 }
