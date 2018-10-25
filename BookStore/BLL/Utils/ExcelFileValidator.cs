b72673f3834d18de12cffb1fd3fea0a98dda9125
Oleksii Prymolonnyi <Oleksii_Prymolonnyi@epam.com>
2018-10-25 19:32:35 +0300

    ImportFunc
    Extends validation

--- BookStore/BLL/Utils/ExcelFileValidator.cs
+++ BookStore/BLL/Utils/ExcelFileValidator.cs
@@ -25,18 +25,18 @@
         {
             using (var package = new ExcelPackage(importSource.InputStream))
             {
-                if (package.Workbook.Worksheets.Count == 0 || package.Workbook.Worksheets.FirstOrDefault().Dimension == null)
+                if (package.Workbook.Worksheets.Count == 0
+                    || package.Workbook.Worksheets.FirstOrDefault().Dimension == null
+                    || !MatchPropertiesAndColumns(package)
+                    || _propertyColumnDictionary.Keys.All(propertyName =>
+                    {
+                        return _propertyColumnDictionary[propertyName] == 0; // in excel column indexes start at 1
+                    }))
                 {
                     return false;
                 }
-
-                MatchPropertiesAndColumns(package);
-
-                return !_propertyColumnDictionary.Keys.All(propertyName =>
-                {
-                    return _propertyColumnDictionary[propertyName] == 0; // in excel column indexes start at 1
-                });
             }
+            return true;
         }
 
         public List<T> Extract(HttpPostedFile importSource)
@@ -48,7 +48,7 @@
             return new List<T>();
         }
 
-        private void MatchPropertiesAndColumns(ExcelPackage package)
+        private bool MatchPropertiesAndColumns(ExcelPackage package)
         {
             var worksheet = package.Workbook.Worksheets.FirstOrDefault();
             for (int column = worksheet.Dimension.Start.Column; column <= worksheet.Dimension.End.Column; column++)
@@ -56,12 +56,18 @@
                 var cellValue = worksheet.Cells[1, column].Value;
                 var cellString = cellValue != null ? cellValue.ToString().Trim() : string.Empty;
 
-                if (_propertyColumnDictionary.ContainsKey(cellString))
+                if (_propertyColumnDictionary.TryGetValue(cellString, out var columnNumber))
                 {
+                    if (columnNumber > 0)
+                    {
+                        return false;
+                    }
+
                     _propertyColumnDictionary.Remove(cellString);
                     _propertyColumnDictionary.Add(cellString, column);
                 }
             }
+            return true;
         }
     }
 }
