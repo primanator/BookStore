2c44bcb071f55df67b79120f0b7b26c3ddc08def
Oleksii Prymolonnyi <Oleksii_Prymolonnyi@epam.com>
2018-11-01 11:28:38 +0200

    Adds file structure check when importing Books from Excel

--- BookStore/BLL/Factory/Implementation/Book/Excel/BookDtoExcelValidator.cs
+++ BookStore/BLL/Factory/Implementation/Book/Excel/BookDtoExcelValidator.cs
@@ -1,19 +1,89 @@
 ﻿namespace BLL.Factory.Implementation.Book.Excel
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Web;
+    using DTO.Entities;
     using Interfaces;
+    using OfficeOpenXml;
 
     public class BookDtoExcelValidator : IValidator
     {
+        private Dictionary<string, int> _propertyColumnDictionary;
+
+        public BookDtoExcelValidator()
+        {
+            var dtoProperties = typeof(BookDto).GetProperties()
+                .Where(prop => !prop.Name.Contains("Id"))
+                .ToList();
+
+            dtoProperties.ForEach(prop => _propertyColumnDictionary.Add(prop.Name, 0));
+        }
+
         public bool CheckStructure(HttpPostedFile source, out string failReason)
         {
-            throw new NotImplementedException();
+            using (var package = new ExcelPackage(source.InputStream))
+            {
+                if (package.Workbook.Worksheets.Count == 0)
+                {
+                    failReason = "Excel file contains no workheets.";
+                    return false;
+                }
+                else if (package.Workbook.Worksheets.FirstOrDefault().Dimension == null)
+                {
+                    failReason = "Workheets contains no cells.";
+                    return false;
+                }
+                else if (!PropertiesAndColumnsMatch(package))
+                {
+                    failReason = "Redundant properties found.";
+                    return false;
+                }
+                else if (PropertiesMissing())
+                {
+                    failReason = "Not all properties found.";
+                    return false;
+                }
+            }
+            failReason = string.Empty;
+            return true;
         }
 
         public bool CheckContent(HttpPostedFile source)
         {
             throw new NotImplementedException();
+        }
+
+        private bool PropertiesAndColumnsMatch(ExcelPackage package)
+        {
+            var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+            for (int column = worksheet.Dimension.Start.Column; column <= worksheet.Dimension.End.Column; column++)
+            {
+                var cellValue = worksheet.Cells[1, column].Value;
+                var cellString = cellValue != null ? cellValue.ToString().Trim() : string.Empty;
+
+                if (_propertyColumnDictionary.TryGetValue(cellString, out var columnNumber))
+                {
+                    if (columnNumber > 0)
+                    {
+                        return false;
+                    }
+
+                    _propertyColumnDictionary.Remove(cellString);
+                    _propertyColumnDictionary.Add(cellString, column);
+                }
+            }
+            return true;
+        }
+
+        private bool PropertiesMissing()
+        {
+            // in excel column indexes start at 1, so if any column number is 0 - corresponding property is missing in import file
+            return _propertyColumnDictionary.Keys.Any(propertyName =>
+            {
+                return _propertyColumnDictionary[propertyName] == 0;
+            });
         }
     }
 }
