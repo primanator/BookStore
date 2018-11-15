c21d953b6e2927711b18d4fb810ef167550efa3d
Oleksii_Prymolonnyi <primolenny.a@gmail.com>
2018-11-15 23:57:50 +0200

    Extends Excel Book Validator.

--- BookStore/BLL/Factory/Implementation/Excel/Books/BookDtoExcelValidator.cs
+++ BookStore/BLL/Factory/Implementation/Excel/Books/BookDtoExcelValidator.cs
@@ -11,6 +11,11 @@
 
     internal class BookDtoExcelValidator : IValidator
     {
+        private enum AlphabetLetters
+        {
+            A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+        };
+
         public event SuccessfulValidationHandler<ValidationEventArgs> ValidatonPassed;
 
         private Dictionary<string, int> _propertyColumnDictionary;
@@ -34,7 +39,8 @@
             var args = new ValidationEventArgs
             {
                 SourceStream = srcStream,
-                SourceMap = _propertyColumnDictionary
+                SourceMap = _propertyColumnDictionary,
+                EntriesAmount = 1 // calculate when validating content
             };
             ValidatonPassed?.Invoke(this, args);
         }
@@ -48,7 +54,7 @@
                     failReason = "Excel file contains no workheets.";
                     return false;
                 }
-                else if (package.Workbook.Worksheets.FirstOrDefault().Dimension == null)
+                else if (package.Workbook.Worksheets.First().Dimension == null)
                 {
                     failReason = "Workheets contains no cells.";
                     return false;
@@ -60,6 +66,7 @@
                 }
                 else if (PropertiesMissing())
                 {
+                    MarkMissingProperties(package);
                     failReason = "Not all properties found.";
                     return false;
                 }
@@ -75,21 +82,20 @@
 
         private bool PropertiesAndColumnsMatch(ExcelPackage package)
         {
-            var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+            var worksheet = package.Workbook.Worksheets.First();
             for (int column = worksheet.Dimension.Start.Column; column <= worksheet.Dimension.End.Column; column++)
             {
-                var cellValue = worksheet.Cells[1, column].Value;
-                var cellString = cellValue != null ? cellValue.ToString().Trim() : string.Empty;
+                var cellValue = worksheet.Cells[1, column].Text.Trim();
 
-                if (_propertyColumnDictionary.TryGetValue(cellString, out var columnNumber))
+                if (_propertyColumnDictionary.TryGetValue(cellValue, out var columnNumber))
                 {
                     if (columnNumber > 0)
                     {
                         return false;
                     }
 
-                    _propertyColumnDictionary.Remove(cellString);
-                    _propertyColumnDictionary.Add(cellString, column);
+                    _propertyColumnDictionary.Remove(cellValue);
+                    _propertyColumnDictionary.Add(cellValue, column);
                 }
             }
             return true;
@@ -97,11 +103,37 @@
 
         private bool PropertiesMissing()
         {
-            // in excel column indexes start at 1, so if any column number is 0 - corresponding property is missing in import file
             return _propertyColumnDictionary.Keys.Any(propertyName =>
             {
+                // in excel column indexes start at 1, so if any column number is 0 - corresponding property is missing in import file
                 return _propertyColumnDictionary[propertyName] == 0;
             });
+        }
+
+        private void MarkMissingProperties(ExcelPackage package)
+        {
+            var missingProperties = _propertyColumnDictionary.Keys.Where(propertyName =>
+            {
+                return _propertyColumnDictionary[propertyName] == 0;
+            }).ToArray();
+
+            var worksheet = package.Workbook.Worksheets.First();
+            for (int column = worksheet.Dimension.Start.Column, propertyIndex = 0; column <= worksheet.Dimension.End.Column; column++)
+            {
+                var cellValue = worksheet.Cells[1, column].Text.Trim();
+                if (string.IsNullOrEmpty(cellValue))
+                {
+                    var columnLetter = ((AlphabetLetters)column - 1); // in excel column indexes start at 1, but in enum - from 0
+                    var cell = worksheet.DataValidations.AddAnyValidation($"A:{columnLetter}");
+                    cell.ShowErrorMessage = true;
+                    cell.ErrorStyle = OfficeOpenXml.DataValidation.ExcelDataValidationWarningStyle.stop;
+                    cell.ErrorTitle = "Book property is missing";
+                    cell.Error = $"Please provide value for {missingProperties[propertyIndex]}";
+                    cell.AllowBlank = false;
+
+                    propertyIndex++;
+                }
+            }
         }
     }
 }
